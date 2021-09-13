bca9f00e905cca60e87029496544280eef4c040f
noxydlfjd <charleshayes@mail.weber.edu>
2021-09-13 08:01:04 -0600

    done

---
+++ 01p-noxydlfjd-main/calculator.py
@@ -0,0 +1,6 @@
+#Charles Hayes
+#Period 4
+num1=int(input('Enter a number:\n'))
+num2=int(input('Enter a second number:\n'))
+print(f"Doing math for {num1} and {num2}.")
+print(f"Addition={num1+num2}\nSubtraction = {num1-num2}\nMultiplication = {num1*num2}\nExponent = {num1**num2}\nDivision = {round((num1/num2),3)}\nFloor Division = {num1//num2}\nModulus Division = {num1%num2}")
